65a59fcdb2883458d62d972f507502883ff0f513
juancolchete <juanudkp@gmail.com>
2024-12-03 19:44:17 -0300

    feat: new contract

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -8,10 +8,10 @@ export const endpoint =
 export const connection = new Connection(endpoint, commitmentLevel);
 
 export const lotteryProgramId = new PublicKey(idl.address);
-export const dev = new PublicKey("CFAUQvT5MA5xJemHwRcbzCQC3UPRyDeQVr1cqo4JbEAp");
-export const mkt = new PublicKey("CvmWdoJiH5qRQywPhNdESMC6LHMEYqYgMvgJMuwFQyQw");
-export const op = new PublicKey("9TFNhwunYo48L5vaW2HLNoCgrwkipy7YZqekAoZABwuK");
-export const burn = new PublicKey("BaD4dbZaxeAGCBhcheWMjV6otw6PcXoG6YW5r7xPzV5v");
+export const dev = new PublicKey("64K98PjEfzLQ4RyZhygrnHA2kX1td2JNtSFtY4ho4EDn");
+export const mkt = new PublicKey("8J1WqJPmQf21hMacA2Q9cMvX7SL8732Ekww5pH53V2rM");
+export const op = new PublicKey("6qKd4QDpdo1EwuAQVpNAYT3M4ixRphNwoVzxzyYNFhao");
+export const burn = new PublicKey("FesdSJ8PfJhdCSfy4xeXvEeiDYvc4uwCQdU73CgeRw41");
 export const token = new PublicKey("HnHYLNZVhn4m1oGv7H4iJm883mn5b69foyRx1ZF5iq1j");
 export const tokenSymbol = "UKN";
 export const MATCHES_6 = new BN(4000);
